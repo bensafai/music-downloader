f3ba75e914cb4e9fd47e30048bd508c3c0dea82b
Ben Safai <ben.safai@zeitakademie.de>
2021-08-17 20:51:39 +0200

    init config service

---
+++ src/Service/ConnectionService/ConfigDataService.py
@@ -0,0 +1,16 @@
+import os.path
+import yaml
+
+class ConfigDataService:
+    def __init__(self, client_name: str):
+        self.config_path = './config.yml'
+        self.client_name = client_name
+
+    def get_config(self):
+        config_data = False
+        file_exists = os.path.isfile(self.config_path)
+        if file_exists:
+            with open(self.config_path) as file:
+                config_file = yaml.full_load(file)
+                config_data = config_file[self.client_name]
+        return config_data
